49de8aceb79f672a81aef30d7b1af3424958e311
EpocDotFr <contact@epoc.fr>
2023-11-19 12:49:35 +0100

    Stage 3

--- app/regex.py
+++ app/regex.py
@@ -1,5 +1,9 @@
 import string
 
+DIGITS_CLASS = string.digits
+LOWER_UPPER_LETTERS_CLASS = string.ascii_letters
+LOWER_UPPER_LETTERS_DIGITS_CLASS = LOWER_UPPER_LETTERS_CLASS + DIGITS_CLASS + '_'
+
 
 def match_pattern(pattern: str, subject: str) -> bool:
     index = 0
@@ -9,10 +13,12 @@ def match_pattern(pattern: str, subject: str) -> bool:
 
         print(index, char)
 
-        if char == '\\':
+        if char == '\\': # Metaclass
             metaclass = pattern[index + 1]
 
-            if metaclass == 'd' and not any(digit in subject for digit in string.digits): # Digits
+            if metaclass == 'd' and not any(digit in subject for digit in DIGITS_CLASS): # Digits
+                return False
+            elif metaclass == 'w' and not any(alnum in subject for alnum in LOWER_UPPER_LETTERS_DIGITS_CLASS): # Alphanumeric
                 return False
 
             index += 1
